8ed01d29701bf8c66164cc6468bc8ed7e55d7ee6
root <root@vm04.(none)>
2016-04-19 13:14:54 +0800

    del oliver

--- OliverAudit/config.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-#!/usr/bin/env python
-
-Net_driver = "eth0"
-OMServer_address = "192.168.2.131"
-Connect_TimeOut = 3
--- OliverServer/config.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-#!/usr/bin/env python
-
-#AUTO_PLATFORM = "ansible"
-#SECRET_KEY = "ctmj#&amp;8hrgow_^sj$ejt@9fzsmh_o)-=(byt5jmg=e3#foya6u"
-SECRET_KEY = "*b$%+mw52yw0_psgevus^m%hjl+!+3l+hgt!2^_okl2c9r)q=a"
